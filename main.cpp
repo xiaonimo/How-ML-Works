f715246f16d95aac5e47e6e9271a3b7e545c228c
ZhiTong <958456112@qq.com>
2018-03-24 21:27:51 +0800

    how to use bp class

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include "network.h"
 #include "func.h"
+#include <ctime>
+double f(double x) {
+    return 1.0/(1+exp(-x));
+}
 
 int main() {
-    BP a(vector<int>{784,100,10});
+    BP a(vector<int>{784, 100 ,10});
 
-    /*
-    print(a.x_index);
-    print(a.y_index);
-    print(a.w_index);
-    print(a.b_index);
-    print(a.weights);
-    print(a.bias);
-    print(a.vals);
-    */
-
-    vector<vector<double>> X(2000, vector<double>(784, 0));
-    vector<vector<double>> Y(2000, vector<double>(10, 0));
+    vector<vector<double>> X(1000, vector<double>(784, 0));
+    vector<vector<double>> Y(1000, vector<double>(10, 0));
     read_mnist(X, Y, "train.csv");
     data_normal(X);
-    a.set_train_data(X, Y);
-    a.train();
+    a.set_train_data(X, Y, 0.8);
+    a.train(10, 3, 1000, 0.01, 0.1, "SGD", "sigmoid");
     a.predict();
 }
