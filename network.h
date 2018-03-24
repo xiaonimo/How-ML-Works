c7b6f73a4c407fcbf7872716367a842ba3692a46
ZhiTong <958456112@qq.com>
2018-03-24 21:28:32 +0800

    bp

--- network.h
+++ network.h
@@ -10,63 +10,56 @@
 
 class BP {
 public:
-    //BP(){}
     BP(vector<int> l);
 
     void set_train_data(vector<vector<double>>, vector<vector<double>>, double r=0.8);
-    //void set_test_data(vector<vector<double>>, vector<vector<double>>);
-    void train(int _batch=10, int _max_itr_all=1, int _max_itr_batch=100,
+    void train(int _batch=10, int _max_itr_all=3, int _max_itr_batch=1000,
                double _learning_rate=0.01, double _min_loss=0.1, string _GD="SGD", string _activation="sigmoid",
                bool _verbose=true);
     void predict(bool _verbose=true);
-    void data_normalization();
-
-    void set_X(vector<double>);
-    void set_Y(vector<double>);
-
 
 public:
     vector<vector<double>> train_X, train_Y;
     vector<vector<double>> test_X, test_Y;
-    vector<double> X, Y;
 
-public:
-    void forword_flow();
-    void backword_flow();
-    void bf_output_hidden1();
-    void bf_hidden1_hidden2();
-    void bf_hidden2_input();
-    void get_loss();
+private:
+    void forword_flow();            //前向传播
+    void backword_flow();           //反向传播
+    void bf_output_hidden1();       //输出层到上一层的梯度
+    void bf_hidden1_input();        //其他层的梯度
+    void get_loss();                //计算当前loss
 
-    void update_weights_bias();
-    void init_weights_bias();
-    double active(double);
-    double active_deri(double);
+    void update_weights_bias();     //根据梯度，更新参数
+    void init_weights_bias();       //参数初始化
+    double active(double);          //激活函数
+    double active_deri(double);     //激活函数导数
 
-public:
-    vector<double> weights;
+private:
+    vector<double> weights;         //权重值
     vector<double> bias;
-    vector<double> d_weights;
-    vector<double> d_bias;
-    vector<double> vals;        //所有层都存储input和output
+    vector<double> d_weights;       //w梯度
+    vector<double> d_bias;          //b梯度
+    vector<double> vals;            //所有层都存储input和output
     
-    vector<int> layers;         //存储每层神经元的个数
-    vector<int> x_index;
+    vector<int> layers;             //存储每层神经元的个数
+    int n_layers;
+    int get_layer_neurals(int);     //获得某一层的节点数
+
+    vector<int> x_index;            //根据某一层获得x,y,w,b的初始index
     vector<int> y_index;
     vector<int> w_index;
     vector<int> b_index;
-
-private:
     int get_x_start(int);
     int get_y_start(int);
     int get_w_start(int);
     int get_b_start(int);
 
-    int get_layer_neurals(int);
+    vector<double> X, Y;            //当前传播的XY
+    void set_X(vector<double>);
+    void set_Y(vector<double>);
 
-    int n_layers;
-
-    int batch;
+private:
+    int batch;                      //各种超参数
     int max_itr_all;
     int max_itr_batch;
     double learning_rate;
@@ -75,7 +68,7 @@ private:
     bool verbose;
 
     string GD;
-    string activation = string("ReLu");
+    string activation;
 };
 
 #endif // NETWORK_H
