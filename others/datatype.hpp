a40e75964120d3fb32195b17d4e58731dde97790
ZhiTong <958456112@qq.com>
2018-04-27 21:36:27 +0800

    data type

---
+++ others/datatype.hpp
@@ -0,0 +1,12 @@
+#ifndef DATATYPE_HPP
+#define DATATYPE_HPP
+
+#include <vector>
+
+typedef double data_t;
+typedef std::vector<data_t> point_t;
+typedef std::vector<point_t> points_t;
+typedef std::size_t index_t;
+typedef std::vector<index_t> indexes_t;
+
+#endif // DATATYPE_HPP
