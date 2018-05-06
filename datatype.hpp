e564e3fe6021eb39efe9f54fb564ebb7420acaa3
ZhiTong <958456112@qq.com>
2018-05-06 19:32:01 +0800

    some typedef

---
+++ datatype.hpp
@@ -0,0 +1,21 @@
+#ifndef DATATYPE_HPP
+#define DATATYPE_HPP
+
+#include <vector>
+
+typedef double data_t;
+typedef std::vector<data_t> point_t;
+typedef std::vector<point_t> points_t;
+typedef std::size_t index_t;
+typedef std::vector<index_t> indexes_t;
+
+typedef std::vector<data_t> vec_t;
+typedef std::vector<vec_t> mat_t;
+
+typedef double param_t;
+typedef std::vector<param_t> vec_param_t;
+typedef std::vector<vec_param_t> mat_param_t;
+
+//typedef const unsigned cunsigned;
+
+#endif // DATATYPE_HPP
